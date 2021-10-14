6c82194c28fd4ad7ea34874ee56fcfa8b8b89ddc
Katrin <katrin@eduroam-169-233-193-20.ucsc.edu>
2021-10-14 12:19:16 -0700

    Initial layout with top section & screen title

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CreateHabitScreen from './app/screens/CreateHabitScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is Social Habit Tracker!</Text>
-      <Text>(Still in very early development)</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CreateHabitScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#5f5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app/screens/CreateHabitScreen.js
+++ app/screens/CreateHabitScreen.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function CreateHabitScreen(props) {
     return (
-        <View>
-            
+        <View style={styles.container}>
+            <View style={styles.top}>
+                <Text style={styles.header}>Create new habit</Text>
+            </View>
+            <View style={styles.bottom}/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    top: {
+        flex: 1,
+        backgroundColor: '#d2efc8',
+        justifyContent: 'flex-end'
+    },
+    bottom: {
+        flex: 3,
+        backgroundColor: '#fff',
+    },
+    header: {
+        fontFamily: 'AvenirNext-Bold',
+        fontSize: 30,
+        justifyContent: 'flex-end',
+        marginLeft: 10,
+    },
+});
 
 export default CreateHabitScreen;

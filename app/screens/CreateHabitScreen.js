c77e8b0cdc6bebca2e418bdec90d21cdd5599d9c
Katrin <katrin@eduroam-169-233-193-20.ucsc.edu>
2021-10-14 12:47:10 -0700

    Better styling and input boxes

--- app/screens/CreateHabitScreen.js
+++ app/screens/CreateHabitScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 function CreateHabitScreen(props) {
     return (
@@ -7,7 +7,28 @@ function CreateHabitScreen(props) {
             <View style={styles.top}>
                 <Text style={styles.header}>Create new habit</Text>
             </View>
-            <View style={styles.bottom}/>
+            <View style={styles.bottom}>
+                <View style={styles.options}>
+                    <Text style={styles.options_font}>Name of habit</Text>
+                    <TextInput style={styles.input} placeholder="enter name"/>
+                </View>
+                <View style={styles.options}>
+                    <Text style={styles.options_font}>Motivation</Text>
+                    <TextInput style={styles.input} placeholder="enter motivation"/>
+                </View>
+                <View style={styles.options}>
+                    <Text style={styles.options_font}>Build or Quit</Text>
+                </View>
+                <View style={styles.options}>
+                    <Text style={styles.options_font}>Habit period</Text>
+                </View>
+                <View style={styles.options}>
+                    <Text style={styles.options_font}>Set a goal</Text>
+                </View>
+                <View style={styles.save_button}>
+                    <Text style={styles.options_font}>Save</Text>
+                </View>
+            </View>
         </View>
     );
 }
@@ -32,6 +53,27 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         marginLeft: 10,
     },
+    options: {
+        flex: 1,
+        marginLeft: 10,
+        marginTop: 10,
+        marginRight: 10,
+    },
+    save_button: {
+        flex: 1,
+        marginLeft: 10,
+        alignSelf: 'center',
+    },
+    options_font: {
+        fontFamily: 'AvenirNext-Medium',
+        fontSize: 18,
+    },
+    input :{
+        borderWidth: .5,
+        height: 30,
+        fontSize: 16,
+        paddingLeft: 5,
+    }
 });
 
 export default CreateHabitScreen;

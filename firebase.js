accd2690ffd71a5b3f7e411dd6ad57388b39a5f7
Katrin <katrin@eduroam-169-233-193-20.ucsc.edu>
2021-11-01 16:27:25 -0700

    removing comment

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 // import {initializeApp} from 'firebase/app';
 // import firebase from 'react-native-firebase';
-// creating test branch
 
 // import * as firebase from "firebase";
 import * as firebase from "./node_modules/firebase/firebase";

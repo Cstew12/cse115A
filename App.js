ecf6a6562c76be442b330fcf1d25a642d36e9789
Katrin <katrin@Katrins-MBP-2.attlocal.net>
2021-11-03 10:32:46 -0700

    Initial commit, app currently only displays profile page for development reasons, avatar circle implemented with text

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RegisterPage from './app/screens/RegisterPage';
 import HabitPage from './app/screens/HabitPage';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import ProfileScreen from './app/screens/ProfileScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -16,10 +17,11 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
+          {/*<Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
           <Stack.Screen name="CreateHabit" component={CreateHabitScreen} />
           <Stack.Screen name="Habits" component={HabitPage} />
-          <Stack.Screen name="Register" options={{headerShown: false}} component={RegisterPage} />
+          <Stack.Screen name="Register" options={{headerShown: false}} component={RegisterPage} />*/}
+          <Stack.Screen name="Profile" options={{headerShown: false}} component={ProfileScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>

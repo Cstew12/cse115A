b9b3765c695b30c6ae43a34b7ce1f5c85274f7d6
comstewa <comstewa@ucsc.edu>
2021-11-02 23:13:52 -0700

    sorta working firebase

--- app/screens/Save.js
+++ app/screens/Save.js
@@ -3,36 +3,33 @@ import { View, TextInput, Image, Button } from 'react-native'
 
 import firebase from 'firebase'
 import { NavigationContainer } from '@react-navigation/native'
-import {db} from "../../firebase";
-import { auth} from "../../firebase";
-import {store} from "../../firebase";
+import {db, store, auth} from "../../firebase";
+
 
 
 export default function Save(props) {
-    const [uri, setUri] = useState('');
-    console.log(props.route.params.image);
-    //const uri = props.image;
-    useEffect(() => {
+    //const [uri, setUri] = useState('');
+    console.log(auth.currentUser.uid);
+    const uri = props.image;
+    /*useEffect(() => {
         const propsUri = props.uri; 
         setUri(uri);
-      });
+      });*/
     const uploadImage = async () => {
         
-        const childPath = `post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`;
-        const response = await fetch(uri);
+        const childPath = `${auth.currentUser.uid}/${Math.random().toString(36)}`;
+        const response = await fetch(props.route.params.image);
         const blob = await response.blob();
 
-        const task = firebase
-            .storage()
+        store
             .ref()
             .child(childPath)
-            .put(blob);
+            .put(blob)
+           // .then( {
+              //  console.log("");
+            //})
 
-        const taskError = snapshot => {
-            console.log(snapshot)
-        }
-
-        task.on("state_changed", taskProgress, taskError, taskCompleted);
+       
     }
 
     return (

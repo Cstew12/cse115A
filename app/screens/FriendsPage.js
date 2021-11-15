b28c4e382f7e7f99e2652e54bd9c0b6193b7cc55
Katrin <katrin@Katrins-MBP-2.attlocal.net>
2021-11-14 22:38:24 -0800

    Created a component for modal

--- app/screens/FriendsPage.js
+++ app/screens/FriendsPage.js
@@ -217,49 +217,7 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'flex-start',
     },
-    centeredView: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 22
-      },
-    modalView: {
-        margin: 5,
-        height: 200, 
-        width: 300, 
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 25,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2
-    },
-    shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-    },
-    button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2
-    },
-    buttonOpen: {
-        backgroundColor: "#F194FF",
-    },
-    buttonClose: {
-        backgroundColor: "#2196F3",
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: "center"
-    },
-    textStyle: {
-        color: "black",
-        fontWeight: "bold",
-        textAlign: "center"
-    },
+    
 });
 
 export default FriendsPage;
